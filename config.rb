6e2c2595efd7d09c2ea09bd2cc7d218383f72ac3
Grant Ammons <gammons@gmail.com>
2015-11-13 09:18:59 -0500

    Use correct READMORE

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ activate :blog do |blog|
   blog.sources = "articles/{year}-{month}-{day}-{title}.html"
   blog.taglink = "tag/{tag}.html"
   # blog.layout = "layout"
-  blog.summary_separator = /(READMORE)/
+  blog.summary_separator = /<!--more-->/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
